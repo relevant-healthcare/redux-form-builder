7ae641933023163ab5261d06596f1628f740f4f7
Wilson Fong <wilson@relevant.healthcare>
2018-09-11 17:53:05 -0400

    remove jsdom copyProps, add ref comment

--- demo/spec/helpers/jsdom.js
+++ demo/spec/helpers/jsdom.js
@@ -1,19 +1,8 @@
 import { jsdom } from 'jsdom';
 
-const dom = new jsdom('<html><body></body></html>');
+// https://github.com/airbnb/enzyme/blob/master/docs/guides/jsdom.md
 global.document = jsdom('');
 global.window = document.defaultView;
 global.navigator = {
   userAgent: 'node.js'
 };
-
-function copyProps(src, target) {
-  const props = Object.getOwnPropertyNames(src)
-    .filter(prop => typeof target[prop] === 'undefined')
-    .reduce((result, prop) => ({
-      ...result,
-      [prop]: Object.getOwnPropertyDescriptor(src, prop),
-    }), {});
-  Object.defineProperties(target, props);
-}
-copyProps(document.defaultView, global);
