1d15ad8bd465c9ae9e46311730ecc1e6402253b2
Wilson Fong <wilson@relevant.healthcare>
2018-09-12 14:46:54 -0400

    remove comment

--- demo/spec/helpers/jsdom.js
+++ demo/spec/helpers/jsdom.js
@@ -1,6 +1,5 @@
 import { jsdom } from 'jsdom';
 
-// https://github.com/airbnb/enzyme/blob/master/docs/guides/jsdom.md
 global.document = jsdom('');
 global.window = document.defaultView;
 global.navigator = {
