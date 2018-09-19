dd5fd01be4372bb3a5199f3986e4b127f439de72
Wilson Fong <wilson@relevant.healthcare>
2018-09-19 16:41:50 -0400

    reset LabeledControl

--- src/components/control.jsx
+++ src/components/control.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import { Popover, OverlayTrigger } from 'react-bootstrap'
-import { HiddenInput } from './input'
+import { TextInput, HiddenInput } from './input'
 import s from 'underscore.string'
-import _ from 'lodash'
 
 function HiddenControl(props) {
   const { id, name, value } = props.inputProps
@@ -25,36 +23,22 @@ function UnlabeledControl(props) {
 
 function LabeledControl(props) {
   const Input = props.input
-  const hasError = !_.isEmpty(props.errors)
-  const popover = _.isEmpty(props.helpPopover) ? null : (
-    <Popover id="popover-trigger-click-root-close" title={props.helpPopover.title}>
-      {props.helpPopover.contents}
-    </Popover>
-  )
-
-  return <div className={`form-group control-group ${hasError ? 'error' : ''}`}>
+  return <div className="form-group">
       <div>
-        <label className="control-label col-sm-2" htmlFor={props.id}>
-          {props.label}{' '}
-          {_.isEmpty(props.helpPopover) ? null : (
-            <OverlayTrigger trigger="click" rootClose placement="right" overlay={popover}>
-              <a href="#" className="label label-info popup-marker">{props.helpPopover.label}</a>
-            </OverlayTrigger>
-          )}
-        </label>
+        <label className="control-label col-sm-2" htmlFor={props.id}>{props.label}</label>
       </div>
-      <div className="controls col-sm-5">
+      <div className="col-sm-5">
         <div>
           {
             <Input {...props.inputProps} />
           }
         </div>
         {
-          hasError ? (
+          _.isEmpty(props.errors) ? null : (
             <span className="help-inline error-inline">
               {s.join(', ', ..._.map(props.errors, 'message'))}
             </span>
-          ) : null
+          )
         }
       </div>
     </div>
