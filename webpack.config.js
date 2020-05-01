ce6b9b38d4c953a86d6528c3840e2cb647ec6094
Tim Mickel <tim@relevant.healthcare>
2020-05-01 12:05:08 -0400

    Do not bundle React 16.5.2 into redux-form-builder

--- webpack.config.js
+++ webpack.config.js
@@ -28,5 +28,19 @@ module.exports = {
   },
   plugins: [
     new webpack.EnvironmentPlugin(['NODE_ENV'])
-  ]
+  ],
+  externals: {
+    react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react'
+    },
+    'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom'
+    }
+  },
 }
